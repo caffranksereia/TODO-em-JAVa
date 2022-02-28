e7a0a534acd0c477fc32c5761fcdb83741e52b9d
ferafael <circuncisaofabio@icloud.com>
2022-02-28 13:59:55 -0300

    modificado para criar o delete,save e update

--- src/main/java/com/TODO/dao/todoDao.java
+++ src/main/java/com/TODO/dao/todoDao.java
@@ -15,7 +15,7 @@ public class todoDao {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/todo","root","");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Todo","root","");
 			
 			
 		}catch (Exception e) {
@@ -23,6 +23,79 @@ public class todoDao {
 		}
 		return con;
 	}
+	
+	public static todo getIdTodo(int id) {
+		todo todo = null;
+		try {
+			Connection con = getConnection();
+			PreparedStatement p = (PreparedStatement) con.prepareStatement("SELECT * FROM todo WHEW ID=?");
+			p.setInt(1, id);
+			ResultSet r = p.executeQuery();
+			
+			while(r.next()) {
+				todo t = new todo();
+				t.setId(r.getInt("id"));
+				t.setTodo(r.getString("todo"));
+				
+			}
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		return todo;
+	}
+	
+	public static int salvar(todo u) {
+		int status = 0;
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement p = (PreparedStatement) con.prepareStatement("insert into todo(todo) values(?)");
+			p.setString(1,u.getTodo());
+			status = p.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}														
+		return status;
+		
+		
+	}
+
+	
+	public static int Update(todo u) {
+		int status = 0;
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement p = (PreparedStatement) con.prepareStatement("UPDATE todo set todo=?");
+			p.setString(1,u.getTodo());
+			status = p.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}														
+		return status;
+		
+		
+	}
+	
+	public static int deletar(todo u) {
+		int status = 0;
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement p = (PreparedStatement) con.prepareStatement("DELETE from todo WHERE id=?");
+			p.setInt(1,u.getId());
+			status = p.executeUpdate();
+		}catch(Exception e) {
+			System.out.println(e);
+		}														
+		return status;
+		
+		
+	}
+	
+	
+	
+	
 	
 	public static List<todo> getAllTodo(){
 		List<todo> list = new ArrayList<todo>();
@@ -40,7 +113,7 @@ public class todoDao {
 			}
 		}catch(Exception e) {
 			System.out.println(e);
-		}
+		}														
 		return list;
 	}
 }
